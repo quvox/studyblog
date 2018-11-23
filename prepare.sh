55195d05ed3df17c4713478702e0a9779fc61ece
Takeshi Kubo <takeshi@quvox.net>
2018-11-23 16:43:43 +0900

    remove theme and use gitsubmodule

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 mkdir -p themes
-cd themes
-git clone https://github.com/digitalcraftsman/hugo-icarus-theme.git
-cd hugo-icarus-theme
+git submodule update --init --recursive
 
-# 自分のgitリポジトリに入れておくため（必須ではない）
-rm -rf .git
-
-cd ../..
 mkdir -p content/post content/about resources static
