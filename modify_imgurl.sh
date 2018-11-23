cef931bff7484a13aed08aa6c9232b1c81322f54
Takeshi Kubo <takeshi@quvox.net>
2018-11-24 02:46:35 +0900

    add tool

---
+++ modify_imgurl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd content/post
+
+for f in *.png; do
+    URL=${PWD}/${f}
+    echo ${URL}
+    TARGET=`grep -l "${URL}" *`
+    echo ${TARGET}
+    if [ ! -z ${TARGET} ]; then
+        sed -i '' -e "s#${URL}#/images/${f}#g" ${TARGET}
+        mv ${f} ../../static/images/
+    fi
+done
